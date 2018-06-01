def4c08607be8cee937a051181c0c93ad2924d97
SuxrobGM <suxrobGM@gmail.com>
2018-06-02 00:09:57 +0500

    added initial files

---
+++ EC_Country_Budget_Editor/MainForm.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace EC_Country_Budget_Editor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.ModPath_TB = new System.Windows.Forms.TextBox();
+            this.SetModPath_Btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Load_Btn = new System.Windows.Forms.Button();
+            this.Save_Btn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.version_label = new System.Windows.Forms.Label();
+            this.Country = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Government_Budget = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Year_Column = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ModPath_TB
+            // 
+            this.ModPath_TB.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ModPath_TB.Location = new System.Drawing.Point(12, 28);
+            this.ModPath_TB.Name = "ModPath_TB";
+            this.ModPath_TB.ReadOnly = true;
+            this.ModPath_TB.Size = new System.Drawing.Size(329, 20);
+            this.ModPath_TB.TabIndex = 0;
+            // 
+            // SetModPath_Btn
+            // 
+            this.SetModPath_Btn.Location = new System.Drawing.Point(340, 26);
+            this.SetModPath_Btn.Name = "SetModPath_Btn";
+            this.SetModPath_Btn.Size = new System.Drawing.Size(38, 23);
+            this.SetModPath_Btn.TabIndex = 1;
+            this.SetModPath_Btn.UseVisualStyleBackColor = true;
+            this.SetModPath_Btn.Click += new System.EventHandler(this.SetModPath_Btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Directory of mod";
+            // 
+            // Load_Btn
+            // 
+            this.Load_Btn.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.Load_Btn.Location = new System.Drawing.Point(12, 514);
+            this.Load_Btn.Name = "Load_Btn";
+            this.Load_Btn.Size = new System.Drawing.Size(75, 23);
+            this.Load_Btn.TabIndex = 4;
+            this.Load_Btn.Text = "Load";
+            this.Load_Btn.UseVisualStyleBackColor = true;
+            this.Load_Btn.Click += new System.EventHandler(this.Load_Btn_Click);
+            // 
+            // Save_Btn
+            // 
+            this.Save_Btn.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.Save_Btn.Location = new System.Drawing.Point(93, 514);
+            this.Save_Btn.Name = "Save_Btn";
+            this.Save_Btn.Size = new System.Drawing.Size(75, 23);
+            this.Save_Btn.TabIndex = 5;
+            this.Save_Btn.Text = "Save";
+            this.Save_Btn.UseVisualStyleBackColor = true;
+            this.Save_Btn.Click += new System.EventHandler(this.Save_Btn_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(235, 511);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Developed by SuxrobGM";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(12, 485);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(370, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Country,
+            this.Government_Budget,
+            this.Year_Column});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 54);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(370, 425);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            // 
+            // version_label
+            // 
+            this.version_label.AutoSize = true;
+            this.version_label.Location = new System.Drawing.Point(313, 526);
+            this.version_label.Name = "version_label";
+            this.version_label.Size = new System.Drawing.Size(69, 13);
+            this.version_label.TabIndex = 9;
+            this.version_label.Text = "version_label";
+            // 
+            // Country
+            // 
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Country.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Country.HeaderText = "Country";
+            this.Country.Name = "Country";
+            this.Country.ReadOnly = true;
+            this.Country.Width = 110;
+            // 
+            // Government_Budget
+            // 
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Government_Budget.DefaultCellStyle = dataGridViewCellStyle3;
+            this.Government_Budget.HeaderText = "Government Budget (mln. dollars)";
+            this.Government_Budget.Name = "Government_Budget";
+            this.Government_Budget.Width = 110;
+            // 
+            // Year_Column
+            // 
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Year_Column.DefaultCellStyle = dataGridViewCellStyle4;
+            this.Year_Column.HeaderText = "Date Scenario";
+            this.Year_Column.Name = "Year_Column";
+            this.Year_Column.ReadOnly = true;
+            this.Year_Column.Width = 105;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(394, 545);
+            this.Controls.Add(this.version_label);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Save_Btn);
+            this.Controls.Add(this.Load_Btn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SetModPath_Btn);
+            this.Controls.Add(this.ModPath_TB);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "EC Country Budget Editor";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ModPath_TB;
+        private System.Windows.Forms.Button SetModPath_Btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Load_Btn;
+        private System.Windows.Forms.Button Save_Btn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label version_label;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Country;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Government_Budget;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Year_Column;
+    }
+}
+
---
+++ EC_Country_Budget_Editor/MainForm.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace EC_Country_Budget_Editor
+{
+    public partial class MainForm : Form
+    {
+        private string ModPath;
+        private string[] CountryFiles;
+        private string Sceanrio_2013 = "2013.11.1";
+        private Dictionary<string, string> CountryNamesWithMoney = new Dictionary<string, string>();
+
+        //private string ModPath { get => ModPath; set => ModPath = value; }
+        /*private string ModPath1
+        {
+            get { return ModPath1 };
+            set { ModPath1 = value};
+        }
+        */
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            version_label.Text = "v. " + Application.ProductVersion;
+        }
+
+        private void SetModPath_Btn_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                dialog.SelectedPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Paradox Interactive", "Hearts of Iron IV", "mod");
+                dialog.Description = "Please set the right directory of the EC mod";
+                dialog.ShowDialog();
+                ModPath = dialog.SelectedPath;
+                ModPath_TB.Text = ModPath;
+
+            }
+        }
+
+        private void Load_Btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ReadData();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }           
+
+            foreach (var item in CountryNamesWithMoney)
+            {
+                dataGridView1.Rows.Add(item.Key, item.Value, Sceanrio_2013);
+            }
+        }
+
+        private void ReadData()
+        {
+            CountryFiles = Directory.GetFiles(ModPath + @"\history\countries\", "*.txt", SearchOption.TopDirectoryOnly);        
+
+            string[] buffer;
+            string money_value = "500"; //Default
+
+            foreach (var file in CountryFiles)
+            {               
+                buffer = File.ReadAllLines(file);
+                
+                foreach (var line in buffer)
+                {
+                    if(line.Contains("variable") && line.Contains("money"))
+                    {
+                        money_value = line.Split('=')[2];
+                        money_value = money_value.Substring(0, money_value.IndexOf('}'));
+                        money_value = money_value.Trim();
+                        break;
+                    }
+                    else
+                    {
+                        money_value = "500";
+                    }
+                }
+                CountryNamesWithMoney.Add(Path.GetFileNameWithoutExtension(file), money_value);
+            }
+        }
+
+        private void Save_Btn_Click(object sender, EventArgs e)
+        {
+            CountryFiles = Directory.GetFiles(ModPath + @"\history\countries\", "*.txt", SearchOption.TopDirectoryOnly);          
+            string money_value = "500"; //Default
+
+            foreach (var file in CountryFiles)
+            {
+                string country_name = Path.GetFileNameWithoutExtension(file);
+                money_value = CountryNamesWithMoney[country_name];
+
+                if (!HasMoneyString(file))
+                {
+                    AddMoneyString(file);
+                }
+
+                ChangeMoney(file, money_value);                                                                    
+            }
+        }
+
+        private void ChangeMoney(string file, string money_value)
+        {       
+            string[] buffer = File.ReadAllLines(file);
+
+            for(var i=0; i<buffer.Length; i++)
+            {
+                if (buffer[i].Contains("variable") && buffer[i].Contains("money"))
+                {
+                    buffer[i] = $"\tadd_to_variable = {{ money = {money_value} }}";
+                }
+            }
+
+            File.WriteAllLines(file, buffer);        
+        }
+
+        private void AddMoneyString(string file, string money_value="500")
+        {
+            string[] buffer = File.ReadAllLines(file);
+            List<string> list = new List<string>(buffer);
+
+            for (var i = 0; i < list.Count; i++)
+            {
+                if (list[i].Contains(Sceanrio_2013) && list[i].Contains("=") && list[i].Contains("{"))
+                {                   
+                    list.Insert(i + 1, $"\tadd_to_variable = {{ money = {money_value} }}");                   
+                    break;
+                }
+            }
+            
+            string[] new_buffer = list.GetRange(0, list.Count).ToArray();
+
+            File.WriteAllLines(file, new_buffer);           
+        }
+
+        private bool HasMoneyString(string file)
+        {
+            string[] buffer = File.ReadAllLines(file);          
+            bool hasMoneyString = false;
+
+            foreach(var line in buffer)
+            {           
+                if (line.Contains("variable") && line.Contains("money"))
+                {
+                    hasMoneyString = true;
+                    break;
+                }
+            }           
+
+            return hasMoneyString;
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            string NewValue = dataGridView1[e.ColumnIndex, e.RowIndex].Value.ToString();
+            string Key = dataGridView1[e.ColumnIndex - 1, e.RowIndex].Value.ToString();
+            CountryNamesWithMoney[Key] = NewValue;
+        }
+    }
+}
